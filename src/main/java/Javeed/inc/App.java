76de612e2b66dbed067eee2dc5ee44581ec041fd
MohdJaveed001 <116237904+MohdJaveed001@users.noreply.github.com>
2022-11-07 16:44:24 +0530

    Update App.java

--- src/main/java/Javeed/inc/App.java
+++ src/main/java/Javeed/inc/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! - Welcome to JaveedIT - best training instiute" );
+        System.out.println( "Hello World! - Welcome to JaveedIT - best training instiute in twin cities" );
     }
 }
